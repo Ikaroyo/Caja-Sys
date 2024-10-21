da04971a0350160026e049fe0637881439a16ebc
Ikaroyo <emmanuel1631@gmail.com>
2024-10-21 18:46:06 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
-import { Tabs, Tab, Box } from "@mui/material";
+import { Tabs, Tab, Box, AppBar } from "@mui/material";
 import Calculadora from "./components/Calculadora";
 import Lotes from "./components/Lotes";
 import ListarLotes from "./components/ListarLotes";
@@ -17,6 +17,16 @@ function App() {
     palette: {
       mode: darkMode ? "dark" : "light",
     },
+    components: {
+      MuiTab: {
+        styleOverrides: {
+          root: {
+            minWidth: "45px",
+            padding: "6px",
+          },
+        },
+      },
+    },
   });
 
   const handleTabChange = (event, newValue) => {
@@ -29,21 +39,32 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Box sx={{ width: "100%" }}>
-        <Tabs
-          value={tabValue}
-          onChange={handleTabChange}
-          variant="fullWidth"
-          sx={{ borderBottom: 1, borderColor: "divider" }}
-        >
-          <Tab icon={<CalculateIcon />} aria-label="Calculadora" />
-          <Tab icon={<ListAltIcon />} aria-label="Lotes" />
-          <Tab icon={<TableChartIcon />} aria-label="Listar" />
-          <ThemeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
-        </Tabs>
-        {tabValue === 0 && <Calculadora />}
-        {tabValue === 1 && <Lotes />}
-        {tabValue === 2 && <ListarLotes />}
+      <Box
+        sx={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <AppBar position="fixed" color="default" elevation={0}>
+          <Tabs
+            value={tabValue}
+            onChange={handleTabChange}
+            variant="fullWidth"
+            sx={{ borderBottom: 1, borderColor: "divider" }}
+          >
+            <Tab icon={<CalculateIcon />} aria-label="Calculadora" />
+            <Tab icon={<ListAltIcon />} aria-label="Lotes" />
+            <Tab icon={<TableChartIcon />} aria-label="Listar" />
+            <ThemeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
+          </Tabs>
+        </AppBar>
+        <Box sx={{ flexGrow: 1, mt: "48px", overflow: "auto" }}>
+          {tabValue === 0 && <Calculadora />}
+          {tabValue === 1 && <Lotes />}
+          {tabValue === 2 && <ListarLotes />}
+        </Box>
       </Box>
     </ThemeProvider>
   );
